19e0d3b2322f817f5f1602e4ff0a5a28834fafb9
rossa1513 <rosa.diaziy@est.iudigital.edu.co>
2024-11-17 15:10:45 -0500

    Descripción de los cambios realizados

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use('/cliente', require('./router/cliente'));
 app.use('/etapa', require('./router/etapa'));
 app.use('/proyecto', require('./router/proyecto'));
-app.use('/universidad', require('./router/universidad'));
 app.use('/tipoProyecto', require('./router/tipoproyecto'));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
